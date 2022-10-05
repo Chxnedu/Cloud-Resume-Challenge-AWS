e4c0094684c24a886ab1b94196bb2c295d43356e
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-10-05 16:40:40 +0100

    made a change

--- main.tf
+++ main.tf
@@ -132,7 +132,6 @@ resource "aws_cloudfront_distribution" "s3resume-distrubution" {
   price_class         = "PriceClass_All"
 
   viewer_certificate {
-    cloudfront_default_certificate = true
     acm_certificate_arn = aws_acm_certificate.domain-cert.arn
     ssl_support_method = "sni-only"
   }
