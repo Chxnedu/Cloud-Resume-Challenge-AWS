39b59f0db5dc7725a8718147fa05d76d707a186d
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-10-05 19:56:53 +0100

    added cypress test

---
+++ cypress/integration/spec1.js
@@ -0,0 +1,8 @@
+describe('Resume Test', () => {
+    it('Visits my resume site and makes sure it loads', () => {
+      cy.visit('https://resume.chxnedu.com')
+  
+      cy.contains('Chinedu Oji')
+  
+    })
+  })
