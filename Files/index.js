02d40e3bef177f9e5316ee989203f627c9b78049
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2022-10-13 17:04:38 +0100

    changed endpoint

--- Files/index.js
+++ Files/index.js
@@ -1,4 +1,4 @@
-fetch('https://qzomn91yyl.execute-api.us-east-1.amazonaws.com/update_count')
+fetch('https://oresm0aeck.execute-api.us-east-1.amazonaws.com/update_count')
 .then(data => { return data.json(); })
 .then(count => { 
     console.log(count.N); 
