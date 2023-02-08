82932893ffcf1a3ec0129d7f3a0acfb5191fb9eb
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2023-02-08 15:57:59 +0100

    new api endpoint

--- Files/index.js
+++ Files/index.js
@@ -1,4 +1,4 @@
-fetch('https://oresm0aeck.execute-api.us-east-1.amazonaws.com/update_count')
+fetch('https://jl0b5q4gk9.execute-api.us-east-1.amazonaws.com')
 .then(data => { return data.json(); })
 .then(count => { 
     console.log(count.N); 
