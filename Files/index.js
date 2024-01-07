0affbe327f0d946dabe2bcc3e2d1a5c52419acfa
Chxn-at-812627843742 <ojichinedu4@gmail.com>
2024-01-07 01:10:42 +0100

    newapi

--- Files/index.js
+++ Files/index.js
@@ -1,4 +1,4 @@
-fetch('https://jl0b5q4gk9.execute-api.us-east-1.amazonaws.com/update_count')
+fetch('https://x2ufdd9eb5.execute-api.us-east-1.amazonaws.com/update_count')
 .then(data => { return data.json(); })
 .then(count => { 
     console.log(count.N); 
